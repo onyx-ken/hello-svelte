1a8da1ef6915afdf338bb25da801bf453fd93065
onyx <bhm1200@gmail.com>
2024-09-02 14:33:32 +0900

    Binding value

---
+++ src/lib/components/CountBinding.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let count = 0;
+
+	export let onCountClick;
+</script>
+
+<button on:click={onCountClick}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -1,2 +1,16 @@
+<script>
+	import CountBinding from '$lib/components/CountBinding.svelte';
+
+	let count = 10
+
+	function onCountClick() {
+		count += 1;
+	}
+</script>
+
 <h1>Welcome to SvelteKit</h1>
+<div class="card-body">
+	<CountBinding {count} {onCountClick}/>
+	<button on:click={onCountClick}>Page Increment Click</button>
+</div>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
